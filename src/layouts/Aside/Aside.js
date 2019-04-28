0afb5fae3b2c246725d0c065cda4eb2b1155ebfe
Isaac-chip <15622125865@163.com>
2019-04-28 19:27:23 +0800

    2019-4-28 更改左侧菜单栏图标。

--- src/layouts/Aside/Aside.js
+++ src/layouts/Aside/Aside.js
@@ -16,7 +16,7 @@ class MyAsideSubMenu extends React.Component {
 
   render() {
     return (
-      <div style={{ width: 200 }}>
+      <div>
         <Menu
           defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
@@ -25,23 +25,23 @@ class MyAsideSubMenu extends React.Component {
           inlineCollapsed={this.state.collapsed}
         >
           <Menu.Item key="1">
-            <Icon type="pie-chart" />
+            <Icon type="sliders" theme="filled" />
             <span>权限管理</span>
           </Menu.Item>
           <Menu.Item key="2">
-            <Icon type="desktop" />
+            <Icon type="setting" theme="filled" />
             <span>角色管理</span>
           </Menu.Item>
           <Menu.Item key="3">
-            <Icon type="inbox" />
+            <Icon type="pushpin" theme="filled" />
             <span>权限列表</span>
           </Menu.Item>
-          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>商品管理</span></span>}>
+          <SubMenu key="sub1" title={<span><Icon type="lock" theme="filled" /><span>商品管理</span></span>}>
             <Menu.Item key="5">商品列表</Menu.Item>
             <Menu.Item key="6">分类参数</Menu.Item>
             <Menu.Item key="7">商品分类</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>数据统计</span></span>}>
+          <SubMenu key="sub2" title={<span><Icon type="fund" theme="filled" /><span>数据统计</span></span>}>
             <Menu.Item key="9">数据报表</Menu.Item>
           </SubMenu>
         </Menu>
