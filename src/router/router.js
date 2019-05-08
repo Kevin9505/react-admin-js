8135818c0c5c6e7cb5ce7d0671b7c10288941951
likaifeng <1980942486@qq.com>
2019-05-08 22:35:45 +0800

    2019-5-8 添加登录页路由重定向。

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch, BrowserRouter as Router, Redirect } from 'react-router-dom'
 
 import Home from '../pages/Home/Home'
 import Login from '../pages/User/Login'
-
 const BasicRoute = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/Home" component={Home} />
+      <Route path='/Login' component={Login} />
+      <Redirect exact from='/' to='/Login' />
+      <Route path="/Home" component={Home}>
+      </Route>
     </Switch>
   </Router>
 )
 
-export default BasicRoute
+export default BasicRoute 
